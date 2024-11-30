8c0a61d3521fd1c211f2b508d8a971d1f9954450
MdAnayatullah <mdanayatullah143@gmail.com>
2024-12-01 01:53:14 +0530

    props concept

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
+import Cards from "./components/Cards"
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <>
+    <div className=" w-full items-center">
+    <h1 className="text-3xl font-bold bg-green-400 rounded-xl px-4 py-2 w-1/6 text-center mx-2 my-4">
       Hello world!
     </h1>
+    </div>
+        <Cards name=" Anayat " surname =" Ansari "/>
+        <Cards name=" Salman " surname= " Khan "/>
+    </>
+    
+    
   )
 }
